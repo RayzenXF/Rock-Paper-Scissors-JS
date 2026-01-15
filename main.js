5354c5c31da418a7cd78a33ed828fdf3bea3bdd6
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-01-15 12:57:11 +0100

    Create "getHumanChoice" function

--- main.js
+++ main.js
@@ -13,7 +13,17 @@ function getComputerChoice() {
     } 
 
     return computerChoice;
+}
+
+function getHumanChoice() {
+
+    const humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+
+    // You do not need to handle reprompting if the user enters an invalid input, 
+    // as that would require things we will teach later.                            --- The Odin Project!
+    // For now, just assume the user will always enter a valid choice.
+
+    return humanChoice;
 
 }
 
-console.log(getComputerChoice());
