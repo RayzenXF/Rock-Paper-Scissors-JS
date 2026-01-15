b7c5e99d26c239701123476a3f93d9124b6f9a8c
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-01-15 12:49:34 +0100

    Move "computerChoice" back inside function, "getComputerNum" changed to const, make a check to choice "Rock, paper, scissors"

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-let getComputerNum;
-let computerChoice;
-
 function getComputerChoice() {
-    getComputerNum = Math.floor(Math.random() * 3) + 1;
+
+    let computerChoice;
+
+    const getComputerNum = Math.floor(Math.random() * 3) + 1;
+
+    if (getComputerNum === 1) {
+        computerChoice = "rock";
+    } else if (getComputerNum === 2) {
+        computerChoice = "paper";
+    } else if (getComputerNum === 3) {
+        computerChoice = "scissors";
+    } 
+
+    return computerChoice;
 
 }
+
+console.log(getComputerChoice());
