2c7f7f0a834c1c48d8e1571e36b5e1964bf117ec
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-01-15 13:01:02 +0100

    Changed variables names from "Choise" to "Selection"

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 function getComputerChoice() {
 
-    let computerChoice;
+    let computerSelection;
 
     const getComputerNum = Math.floor(Math.random() * 3) + 1;
 
     if (getComputerNum === 1) {
-        computerChoice = "rock";
+        computerSelection = "rock";
     } else if (getComputerNum === 2) {
-        computerChoice = "paper";
+        computerSelection = "paper";
     } else if (getComputerNum === 3) {
-        computerChoice = "scissors";
+        computerSelection = "scissors";
     } 
 
-    return computerChoice;
+    return computerSelection;
 }
 
 function getHumanChoice() {
 
-    const humanChoice = prompt("Rock, Paper, Scissors?").toLowerCase();
+    const humanSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
 
     // You do not need to handle reprompting if the user enters an invalid input, 
     // as that would require things we will teach later.                            --- The Odin Project!
     // For now, just assume the user will always enter a valid choice.
 
-    return humanChoice;
+    return humanSelection;
 
 }
 
