a255b53518438d7b1036bbda53b27085e3b0cf8d
RayzenXF <227984918+RayzenXF@users.noreply.github.com>
2026-02-13 13:16:41 +0100

    Fix spaces

--- main.js
+++ main.js
@@ -1,114 +1,114 @@
-    const rockBtn = document.querySelector("#rock-btn");
-    const paperBtn = document.querySelector("#paper-btn");
-    const scissorsBtn = document.querySelector("#scissors-btn");
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
 
-    const resetBtn = document.querySelector("#reset-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
-    const consoleGame = document.querySelector("#console-game");
-    const consolePara = document.querySelector("#console-para");
+const consoleGame = document.querySelector("#console-game");
+const consolePara = document.querySelector("#console-para");
 
-    const humanScoreText = document.querySelector("#human-score");
-    const computerScoreText = document.querySelector("#computer-score");
+const humanScoreText = document.querySelector("#human-score");
+const computerScoreText = document.querySelector("#computer-score");
+
+resetBtn.disabled = true;
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = "";
+let computerChoice = "";
+
+function getComputerChoice() {
+
+    let computerSelection;
+
+    const getComputerNum = Math.floor(Math.random() * 3) + 1;
+
+    if (getComputerNum === 1) {
+        computerSelection = "rock";
+    } else if (getComputerNum === 2) {
+        computerSelection = "paper";
+    } else if (getComputerNum === 3) {
+        computerSelection = "scissors";
+    }
+
+    return computerSelection;
+}
+
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+
+    resetBtn.disabled = false;
+};
+
+function enableButtons() {
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
 
     resetBtn.disabled = true;
+};
 
-    let humanScore = 0;
-    let computerScore = 0;
+function checkWin() {
 
-    let humanChoice = "";
-    let computerChoice = "";
-
-    function getComputerChoice() {
-
-        let computerSelection;
-
-        const getComputerNum = Math.floor(Math.random() * 3) + 1;
-
-        if (getComputerNum === 1) {
-            computerSelection = "rock";
-        } else if (getComputerNum === 2) {
-            computerSelection = "paper";
-        } else if (getComputerNum === 3) {
-            computerSelection = "scissors";
-        }
-
-        return computerSelection;
+    if (humanScore === 5) {
+        consolePara.textContent = `Hooray! You won 5 rounds! Congratulations!`;
+        disableButtons();
+    } else if (computerScore === 5) {
+        consolePara.textContent = `Ohhh... Computer won 5 rounds. Try again!`;
+        disableButtons();
     }
+};
 
-    function disableButtons() {
-        rockBtn.disabled = true;
-        paperBtn.disabled = true;
-        scissorsBtn.disabled = true;
+function playRound(computerChoice) {
 
-        resetBtn.disabled = false;
-    };
-
-    function enableButtons() {
-        rockBtn.disabled = false;
-        paperBtn.disabled = false;
-        scissorsBtn.disabled = false;
-
-        resetBtn.disabled = true;
-    };
-
-    function checkWin() {
-
-        if (humanScore === 5) {
-            consolePara.textContent = `Hooray! You won 5 rounds! Congratulations!`;
-            disableButtons();
-        } else if (computerScore === 5) {
-            consolePara.textContent = `Ohhh... Computer won 5 rounds. Try again!`;
-            disableButtons();
-        }
-    };
-
-    function playRound(computerChoice) {
-
-        if (humanChoice === computerChoice) {
-            consolePara.textContent = "Draw!";
-            checkWin();
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
-            humanScore++;
-            humanScoreText.textContent = humanScore;
-            checkWin();
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
-            humanScore++;
-            humanScoreText.textContent = humanScore;
-            checkWin();
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
-            humanScore++;
-            humanScoreText.textContent = humanScore;
-            checkWin();
-        } else {
-            consolePara.textContent = `Oops, Computer Won! Computer's ${computerChoice} beats your ${humanChoice}!`;
-            computerScore++;
-            computerScoreText.textContent = computerScore;
-            checkWin();
-        }
+    if (humanChoice === computerChoice) {
+        consolePara.textContent = "Draw!";
+        checkWin();
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
+        humanScore++;
+        humanScoreText.textContent = humanScore;
+        checkWin();
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
+        humanScore++;
+        humanScoreText.textContent = humanScore;
+        checkWin();
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        consolePara.textContent = `You Won! Your ${humanChoice} beats computer's ${computerChoice}!`;
+        humanScore++;
+        humanScoreText.textContent = humanScore;
+        checkWin();
+    } else {
+        consolePara.textContent = `Oops, Computer Won! Computer's ${computerChoice} beats your ${humanChoice}!`;
+        computerScore++;
+        computerScoreText.textContent = computerScore;
+        checkWin();
     }
+}
 
-    rockBtn.addEventListener("click", () => {
-        humanChoice = "rock";
-        computerChoice = getComputerChoice();
-        playRound(computerChoice);
-    });
+rockBtn.addEventListener("click", () => {
+    humanChoice = "rock";
+    computerChoice = getComputerChoice();
+    playRound(computerChoice);
+});
 
-    paperBtn.addEventListener("click", () => {
-        humanChoice = "paper";
-        computerChoice = getComputerChoice();
-        playRound(computerChoice);
-    });
+paperBtn.addEventListener("click", () => {
+    humanChoice = "paper";
+    computerChoice = getComputerChoice();
+    playRound(computerChoice);
+});
 
-    scissorsBtn.addEventListener("click", () => {
-        humanChoice = "scissors";
-        computerChoice = getComputerChoice();
-        playRound(computerChoice);
-    });
+scissorsBtn.addEventListener("click", () => {
+    humanChoice = "scissors";
+    computerChoice = getComputerChoice();
+    playRound(computerChoice);
+});
 
-    resetBtn.addEventListener("click", () => {
+resetBtn.addEventListener("click", () => {
     enableButtons();
     humanScore = 0;
     computerScore = 0; 
@@ -116,4 +116,4 @@
     humanScoreText.textContent = humanScore;
     computerScoreText.textContent = computerScore;
     consolePara.textContent = `Good luck!`;
-    });
+});
